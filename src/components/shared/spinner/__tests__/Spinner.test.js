2d78e1c2d2302d56345ae55a2f0864847de9c832
Oleksii Trekhleb <trehleb@gmail.com>
2019-01-24 18:08:56 +0500

    Cover Spinner with tests.

---
+++ src/components/shared/spinner/__tests__/Spinner.test.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import { Spinner } from '../Spinner';
+
+describe('Spinner', () => {
+  it('should be rendered correctly', () => {
+    const tree = renderer
+      .create(<Spinner/>)
+      .toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
