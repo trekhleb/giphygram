8534859820d00d5f36421644f3bf091cbdc7d363
Oleksii Trekhleb <trehleb@gmail.com>
2019-01-24 06:40:39 +0500

    Add build folder to GitHub in order to be displayed on GH Pages.

---
+++ build/precache-manifest.88cbf0b50fb6f6d47f920a4c1df45916.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "d299f83383581e860073",
+    "url": "/static/css/main.0b85607f.chunk.css"
+  },
+  {
+    "revision": "d299f83383581e860073",
+    "url": "/static/js/main.d299f833.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "d063ddd457dc0e1bf23c22d4839ecb43",
+    "url": "/index.html"
+  }
+];
