3cdb1e57c14ee5b67af15c732f3b82fecf6c444a
Oleksii Trekhleb <trehleb@gmail.com>
2019-01-24 09:49:01 +0500

    Add link to GIPHY to footer.

--- src/components/shared/Footer.js
+++ src/components/shared/Footer.js
@@ -3,9 +3,9 @@ import React from 'react';
 export class Footer extends React.Component {
   render() {
     return (
-      <div>
-        Footer text
-      </div>
+      <small className="text-muted">
+        Images are taken from <a href="https://giphy.com/">GIPHY</a>
+      </small>
     );
   }
 }
