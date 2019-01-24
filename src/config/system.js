bd259ec2416ea498fe288470b7368ceae1e3fd7c
Oleksii Trekhleb <trehleb@gmail.com>
2019-01-24 14:40:09 +0500

    Increase batch size.

--- src/config/system.js
+++ src/config/system.js
@@ -4,4 +4,4 @@ export const GIPHY_API_HOST = 'https://api.giphy.com';
 // We store Giphy API Key like this just for demo purposes to be able to use GitHub Pages for demo.
 export const GIPHY_API_KEY = 'CdRKiCMbTnt9CkZTZ0lGukSczk6iT4Z6';
 
-export const GIPHY_BATCH_SIZE = 20;
+export const GIPHY_BATCH_SIZE = 30;
