31b797729988ad6a9f230397f38a03b6cae96a39
Oleksii Trekhleb <trehleb@gmail.com>
2019-01-24 07:02:38 +0500

    Clean up default project files and do some folder refactoring.

---
+++ src/components/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <header>
+          <p>
+            App here.
+          </p>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
