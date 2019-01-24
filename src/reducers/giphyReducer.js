0543a669620aeaca04f410963934831b9ab14858
Oleksii Trekhleb <trehleb@gmail.com>
2019-01-24 09:26:18 +0500

    Create Giphy reducer.

---
+++ src/reducers/giphyReducer.js
@@ -0,0 +1,43 @@
+import { GIPHY_ACTION_TYPES } from '../actions/giphyActions';
+import { PENDING, FULFILLED, REJECTED } from 'redux-promise-middleware';
+
+const initialState = {
+  data: [],
+  pagination: {},
+  meta: {},
+  isLoading: false,
+  error: null,
+};
+
+export const giphyReducer = (state = initialState, action) => {
+  switch (action.type) {
+
+    case GIPHY_ACTION_TYPES.GIPHY_SEARCH + '_' + PENDING:
+      return {
+        ...state,
+        isLoading: true,
+        error: null,
+      };
+
+    case GIPHY_ACTION_TYPES.GIPHY_SEARCH + '_' + FULFILLED:
+      const { data, pagination, meta } = action.payload;
+
+      return {
+        ...state,
+        data,
+        pagination,
+        meta,
+        isLoading: false,
+        error: null,
+      };
+
+    case GIPHY_ACTION_TYPES.GIPHY_SEARCH + '_' + REJECTED:
+      return {
+        ...initialState,
+        error: null,
+      };
+
+    default:
+      return state;
+  }
+};
