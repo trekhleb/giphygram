491eeb04b090e21276019ee24b86a8a37539c5f1
Oleksii Trekhleb <trehleb@gmail.com>
2019-01-24 18:07:18 +0500

    Cover Header with tests.

---
+++ src/components/shared/header/__tests__/Header.test.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import { Header } from '../Header';
+
+jest.mock('../../../../containers/searchForm/SearchFormContainer', () => ({
+  SearchFormContainer: 'SearchFormContainer',
+}));
+
+describe('Header', () => {
+  it('should be rendered correctly', () => {
+    const tree = renderer
+      .create(<Header/>)
+      .toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
