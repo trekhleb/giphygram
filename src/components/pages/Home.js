45366ba5dbe08efd7adff7e95593e856ae9292b0
Oleksii Trekhleb <trehleb@gmail.com>
2019-01-24 08:39:45 +0500

    Create Giphy configs.

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,12 +1,10 @@
 import React from 'react';
-import { SearchForm } from '../searchForm/SearchForm';
 import { GifList } from '../gifList/GifList';
 
 export class Home extends React.Component {
   render() {
     return (
       <div>
-        <SearchForm/>
         <GifList/>
       </div>
     );
---
+++ src/config/system.js
@@ -0,0 +1,5 @@
+export const GIPHY_API_HOST = 'https://api.giphy.com';
+
+// @TODO: GIPHY_API_KEY must be taken from environment variables like 'process.env.GIPHY_API_KEY'.
+// We store Giphy API Key like this just for demo purposes to be able to use GitHub Pages for demo.
+export const GIPHY_API_KEY = 'CdRKiCMbTnt9CkZTZ0lGukSczk6iT4Z6';
