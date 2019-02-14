80c5b1f4fbbb934639a2f789be8e0fc79f76a95e
Oleksii Trekhleb <trehleb@gmail.com>
2019-02-14 08:07:52 +0200

    Cleanup store file.

--- src/store.js
+++ src/store.js
@@ -7,10 +7,7 @@ import { rootReducer } from './reducers/rootReducer';
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 // Add middleware to be able to use promises and chains of actions.
-const middleware = applyMiddleware(
-  promise(),
-  thunk,
-);
+const middleware = applyMiddleware(promise(), thunk);
 
 // Create Redux store for the application.
 export const store = createStore(
