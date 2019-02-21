1fb3820c7f39d003c308166c0f6e6714619d6b1e
Oleksii Trekhleb <trehleb@gmail.com>
2019-02-21 06:15:54 +0200

    Cover SearchSummary component with tests.

---
+++ src/components/searchSummary/__tests__/SearchSummary.test.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import { SearchSummary } from '../SearchSummary';
+
+describe('SearchSummary', () => {
+  it('should be rendered correctly', () => {
+    const tree = renderer
+      .create(<SearchSummary total={42} />)
+      .toJSON();
+
+    expect(tree).toMatchSnapshot();
+  });
+
+  it('should not be rendered if there are no search items', () => {
+    const tree = renderer
+      .create(<SearchSummary />)
+      .toJSON();
+
+    expect(tree).toMatchSnapshot();
+  });
+});
