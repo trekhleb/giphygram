f9828e701ff795a1eeb35a79eeb02d241d8879ce
Oleksii Trekhleb <trehleb@gmail.com>
2019-02-25 13:21:45 +0200

    Cover SearchSummaryContainer with tests.

--- src/containers/searchSummaryContainer/__tests__/SearchSummaryContainer.test.js
+++ src/containers/searchSummaryContainer/__tests__/SearchSummaryContainer.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
-import { SearchSummaryContainer } from '../SearchSummaryContainer';
+import SearchSummaryContainerConnected, { SearchSummaryContainer } from '../SearchSummaryContainer';
 
 jest.mock(
   '../../../components/searchSummary/SearchSummary',
@@ -9,10 +9,39 @@ jest.mock(
   }),
 );
 
+jest.mock('react-redux', () => ({
+  connect: mapStateToProps => (Component) => {
+    const state = {
+      searchResults: {
+        data: [],
+        pagination: {
+          total_count: 100,
+        },
+        meta: {},
+        isLoading: false,
+        isFetchingMore: false,
+        error: null,
+      },
+    };
+    const propsFromState = mapStateToProps(state);
+    return () => <Component {...propsFromState} />;
+  },
+}));
+
 describe('SearchSummaryContainer', () => {
   it('should be rendered correctly by default', () => {
     const tree = renderer
       .create(<SearchSummaryContainer />)
+      .toJSON();
+
+    expect(tree).toMatchSnapshot();
+  });
+
+  it('should be rendered and connected correctly by default', () => {
+    const tree = renderer
+      .create((
+        <SearchSummaryContainerConnected />
+      ))
       .toJSON();
 
     expect(tree).toMatchSnapshot();
