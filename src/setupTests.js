3c569292d26a6118b0dc974bca3ca41197614fa5
Oleksii Trekhleb <trehleb@gmail.com>
2019-02-28 07:25:56 +0200

    Add responsive columns support.

--- src/setupTests.js
+++ src/setupTests.js
@@ -3,4 +3,16 @@ import { configure } from 'enzyme';
 // eslint-disable-next-line import/no-extraneous-dependencies
 import Adapter from 'enzyme-adapter-react-16';
 
+// Configure Enzyme.
 configure({ adapter: new Adapter() });
+
+// Setup global mocks.
+
+// Mock window.matchMedia() method.
+global.matchMedia = global.matchMedia || jest.fn().mockImplementation(query => ({
+  matches: true,
+  media: query,
+  onchange: null,
+  addListener: jest.fn(),
+  removeListener: jest.fn(),
+}));
