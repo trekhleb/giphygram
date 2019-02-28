40e63e979444e9b977bd63d3ec65aee844264724
Oleksii Trekhleb <trehleb@gmail.com>
2019-02-28 08:08:02 +0200

    Add responsive columns support.

--- src/components/shared/layout/Layout.js
+++ src/components/shared/layout/Layout.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { SCREEN_SIZES } from '../../../services/LayoutService';
 
 export class Layout extends React.Component {
   static propTypes = {
@@ -7,7 +8,50 @@ export class Layout extends React.Component {
       PropTypes.arrayOf(PropTypes.node),
       PropTypes.node,
     ]).isRequired,
+    onMediaQueryChange: PropTypes.func,
   };
+
+  static defaultProps = {
+    onMediaQueryChange: () => {},
+  };
+
+  constructor(props) {
+    super(props);
+
+    // List of all listeners to media query changes.
+    this.mediaQueries = {};
+  }
+
+  componentDidMount() {
+    const { onMediaQueryChange } = this.props;
+    let initialScreenSize = null;
+
+    Object
+      .values(SCREEN_SIZES)
+      .forEach((screenSize) => {
+        // Try to match current media query.
+        this.mediaQueries[screenSize.id] = window.matchMedia(screenSize.mediaQuery);
+        if (this.mediaQueries[screenSize.id].matches) {
+          initialScreenSize = screenSize;
+        }
+
+        // Subscribe to media query changes.
+        this.mediaQueries[screenSize.id].addListener(
+          (screenSizeEntity => (e) => {
+            if (e.matches) {
+              onMediaQueryChange(screenSizeEntity.id);
+            }
+          })(screenSize),
+        );
+      });
+
+    // Setup initial media query match.
+    onMediaQueryChange(initialScreenSize.id);
+  }
+
+  componentWillUnmount() {
+    Object.values(this.mediaQueries).forEach(mediaQuery => mediaQuery.removeListener());
+  }
 
   render() {
     const { children } = this.props;
